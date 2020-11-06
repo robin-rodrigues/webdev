988eba92141dc59636e38762e7e2e75b5a0c3828
Rodrob-93 <robinrodrigues6499@gmail.com>
2020-11-06 18:25:15 +0530

    Added more backdrop animations (part 2)

--- framer-motion/pizzajoint/src/components/Modal.js
+++ framer-motion/pizzajoint/src/components/Modal.js
@@ -11,6 +11,20 @@ const backdrop = {
     }
 }
 
+const modal = {
+    hidden: {
+        y: "-100vh",
+        opacity: 0
+    },
+    visible: {
+        y: "200px", 
+        opacity: 1,
+        transition: { 
+            delay: 0.5 
+        } 
+    }
+}
+
 const Modal = ({ showModal, setShowModal }) => {
     return (
         <AnimatePresence exitBeforeEnter>
@@ -19,7 +33,16 @@ const Modal = ({ showModal, setShowModal }) => {
                     variants={backdrop}
                     initial="hidden"
                     animate="visible"
+                    exit="hidden"
                 >
+                    <motion.div className="modal"
+                        variants={modal}
+                    >
+                        <p>Want to make another pizza?</p>
+                        <Link to="/">
+                            <button>Start Again</button>
+                        </Link>
+                    </motion.div>
                     
                 </motion.div>                
             )}
